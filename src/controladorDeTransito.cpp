1eadbea95138773a4e38e370198f7b1e6f90c69c
Ana Luiza <analuizamedeiros1314@gmail.com>
2024-08-24 17:47:15 -0300

    :sparkles: feat: Função de Relatar Estado

--- src/controladorDeTransito.cpp
+++ src/controladorDeTransito.cpp
@@ -122,4 +122,37 @@ void ControladorDeTransito::avancarHoras(int horas)
 
 void ControladorDeTransito::relatarEstado()
 {
+  //Relatar onde está cada pessoa
+  cout << "Relatório do local de cada passageiro" << endl;
+  for(const auto& passageiro: listaPassageiros){
+    if(passageiro->getEmTransito()){
+      cout << passageiro->getNome() << " está em trânsito." << endl;
+      //acrescentar os outros dados
+    }
+    else{
+      cout << passageiro->getNome() << " está em " << passageiro->getLocalAtual()->getNome() << endl;
+    }
+  }
+
+  //Relatar onde está cada transporte
+  cout << "Relatório do local de cada transporte" << endl;
+  for (const auto& transporte : listaTransportes) {
+    if (transporte->getEmTransito()) {
+      cout << transporte->getNome() << " está em trânsito." << endl;
+    }
+    else{
+      cout << transporte->getNome() << " está em " << transporte->getLocalAtual()->getNome() << endl;
+    }
+  }
+
+  //Relatar todas as viagens em andamento
+  cout << "Relatório de todas as viagens em andamento" << endl;
+  for (const auto& viagem : listaViagens) {
+        viagem->relatarEstado();
+  }
+
+  //Relatar todas as cidade mais visitadas
+  cout << "Relatório das cidades mais visitadas" << endl;
+  //A se fazer
+
 }
